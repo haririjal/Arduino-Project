a224069a4bfaee887a0d5bdf72b6aa7d8764e798
Hari Rijal <reezalharee2015@gmail.com>
2019-12-27 12:24:54 +0000

    Update _7segment.ino, _09seg.ino, abcdefg.ino, buzzerDemo.ino, laserDemo.ino, motro_sarara.ino, rbg_demo.ino, rgb_led.ino, toggleLed.ino, ultrasonicsensor.ino files

---
+++ laserDemo.ino
@@ -0,0 +1,27 @@
+void setup() {
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
+  
+}
+
+void loop() {
+  int display;
+  for(display=2;display<=9;display++)
+  {
+    digitalWrite(display,1);
+    
+    delay(200);
+    
+  } 
+  for(display=9;display>=2;display--){
+    digitalWrite(display,0);
+    delay(200);
+  }
+
+}
